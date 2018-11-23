c24230a99c28f93f9214ea69d3c47969b0bd2083
Dominik Januszewicz <Dominik.Januszewicz.PL@ieee.org>
2018-11-23 18:46:38 +0100

    chat app boilerplate code

---
+++ src/main/java/pl/mgr/hs/chat/ChatApp.java
@@ -0,0 +1,14 @@
+package pl.mgr.hs.chat;
+
+
+import java.util.Scanner;
+
+/** Created by dominik on 16.11.18. */
+public class ChatApp {
+
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+  }
+}
